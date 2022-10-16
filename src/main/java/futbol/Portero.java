926e200b6269486866c01430dde2b6ecb62c730c
primolechuga <juanestebaca123@gmail.com>
2022-10-16 17:39:53 -0500

    JAVA

--- src/main/java/futbol/Portero.java
+++ src/main/java/futbol/Portero.java
@@ -4,7 +4,7 @@ public class Portero extends Futbolista{
     public byte dorsal;
 
     public Portero(String nombre,int edad,short golesRecibidos, byte dorsal){
-        super(nombre,edad,"portero");
+        super(nombre,edad,"Portero");
         this.golesRecibidos=golesRecibidos;
         this.dorsal=dorsal;
     }
